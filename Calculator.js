2d54135a2600262e41517008b1e6feeb5b8e9b34
Satya Ajay <86813260+rsa914016@users.noreply.github.com>
2023-03-09 08:56:46 +0000

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,20 @@
+function add (a, b){
+    console.log(`${a} + ${b} = ${a+b}`)
+}
+
+
+function sub (a, b){
+    console.log(`${a} - ${b} = ${a-b}`)
+}
+
+
+function mul (a, b){
+    console.log(`${a} * ${b} = ${a*b}`)
+}
+
+
+function div (a, b){
+    console.log(`${a} ÷ ${b} = ${a/b}`)
+}
+
+module.exports = {add, sub, mul, div}
